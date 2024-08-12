a3d3027b7fce8fe014c39007bb9f1c7414a396e0
jus1d <artfa63@gmail.com>
2024-08-12 14:24:53 +0400

    chore: Refactoring

--- index.js
+++ index.js
@@ -1,98 +1,46 @@
-var circle = function (ctx, cx, cy, radius, style) {
-    ctx.beginPath();
-    ctx.arc(cx, cy, radius, 0, Math.PI * 2);
-    ctx.fillStyle = style;
-    ctx.fill();
-    ctx.closePath();
-};
-var uuid = function () {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-        var randomValue = (Math.random() * 16) | 0;
-        var value = c === "x" ? randomValue : (randomValue & 0x3) | 0x8;
-        return value.toString(16);
-    });
-};
+var COLORS = [
+    "#01BAEF",
+    "#FFF275",
+    "#DBBADD",
+    "#643A71",
+    "#C4D6B0",
+    "#AA4465",
+];
 var choose = function (arr) {
     if (arr.length === 0)
         return undefined;
     var idx = Math.floor(Math.random() * arr.length);
     return arr[idx];
 };
-var translateCanvasToScreenCoordinates = function (canvas, x, y) {
-    var rect = canvas.getBoundingClientRect();
-    return [x + rect.left, y + rect.top];
+var id = function () {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+        var randomValue = (Math.random() * 16) | 0;
+        var value = c === "x" ? randomValue : (randomValue & 0x3) | 0x8;
+        return value.toString(16);
+    });
+};
+var drawCircle = function (ctx, cx, cy, radius, style) {
+    ctx.beginPath();
+    ctx.arc(cx, cy, radius, 0, Math.PI * 2);
+    ctx.fillStyle = style;
+    ctx.fill();
+    ctx.closePath();
 };
 (function () {
-    // localStorage.clear();
-    var _a;
     var canvas = document.getElementById("window");
-    if (canvas === null) {
-        throw new Error("Could not find window");
-    }
+    if (canvas === null)
+        throw new Error("Canvas with id `window` not found");
     var ctx = canvas.getContext("2d");
-    if (ctx === null) {
-        throw new Error("Unsupported '2d' context");
-    }
-    var COLORS = [
-        "#01BAEF",
-        "#FFF275",
-        "#DBBADD",
-        "#643A71",
-        "#C4D6B0",
-        "#AA4465",
-    ];
-    var ID = uuid();
-    var COLOR = (_a = choose(COLORS)) !== null && _a !== void 0 ? _a : "magenta";
+    if (ctx === null)
+        throw new Error("2d context not supported");
+    var COLOR = choose(COLORS) || "magenta";
+    var ID = id();
     window.onresize = function () {
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = window.innerHeight;
-        var _a = translateCanvasToScreenCoordinates(ctx.canvas, ctx.canvas.width / 2, ctx.canvas.height / 2), x = _a[0], y = _a[1];
-        var sessions = getSessions() || new Array();
-        updateSession(sessions, { id: ID, color: COLOR, x: x, y: y });
-        sessions = getSessions() || new Array();
-        console.log(sessions);
     };
-    window.addEventListener("beforeunload", function () {
-        var sessions = getSessions();
-        if (!sessions)
-            return;
-        sessions = sessions.filter(function (s) { return s.id != ID; });
-        saveSessions(sessions);
-    });
-    var getSessions = function () {
-        var sessions = localStorage.getItem("sessions");
-        if (sessions) {
-            return JSON.parse(sessions);
-        }
-        return null;
-    };
-    var saveSessions = function (sessions) {
-        var json = JSON.stringify(sessions);
-        localStorage.setItem("sessions", json);
-    };
-    var updateSession = function (sessions, session) {
-        for (var i = 0; i < sessions.length; i++) {
-            if (sessions[i].id === ID) {
-                sessions[i] = session;
-            }
-        }
-        return sessions;
-    };
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    var _b = translateCanvasToScreenCoordinates(ctx.canvas, ctx.canvas.width / 2, ctx.canvas.height / 2), x = _b[0], y = _b[1];
-    var session = {
-        id: uuid(),
-        color: COLOR,
-        x: x,
-        y: y,
-    };
-    var sessions = getSessions() || new Array();
-    sessions.push(session);
-    saveSessions(sessions);
-    console.log(getSessions());
     var render = function () {
-        circle(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2, 100, COLOR);
+        drawCircle(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2, 100, COLOR);
         requestAnimationFrame(render);
     };
     render();
--- index.ts
+++ index.ts
@@ -1,33 +1,11 @@
-interface Session {
-  id: string;
-  color: string;
-  x: number;
-  y: number;
-}
-
-const circle = (
-  ctx: CanvasRenderingContext2D,
-  cx: number,
-  cy: number,
-  radius: number,
-  style: string,
-) => {
-  ctx.beginPath();
-
-  ctx.arc(cx, cy, radius, 0, Math.PI * 2);
-  ctx.fillStyle = style;
-  ctx.fill();
-
-  ctx.closePath();
-};
-
-const uuid = (): string => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    const randomValue = (Math.random() * 16) | 0;
-    const value = c === "x" ? randomValue : (randomValue & 0x3) | 0x8;
-    return value.toString(16);
-  });
-};
+const COLORS: Array<string> = [
+  "#01BAEF",
+  "#FFF275",
+  "#DBBADD",
+  "#643A71",
+  "#C4D6B0",
+  "#AA4465",
+];
 
 const choose = <T>(arr: Array<T>): T | undefined => {
   if (arr.length === 0) return undefined;
@@ -36,118 +14,45 @@ const choose = <T>(arr: Array<T>): T | undefined => {
   return arr[idx];
 };
 
-const translateCanvasToScreenCoordinates = (
-  canvas: HTMLCanvasElement,
-  x: number,
-  y: number,
-): [number, number] => {
-  const rect = canvas.getBoundingClientRect();
+const id = (): string => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const randomValue = (Math.random() * 16) | 0;
+    const value = c === "x" ? randomValue : (randomValue & 0x3) | 0x8;
+    return value.toString(16);
+  });
+};
 
-  return [x + rect.left, y + rect.top];
+const drawCircle = (
+  ctx: CanvasRenderingContext2D,
+  cx: number,
+  cy: number,
+  radius: number,
+  style: string,
+) => {
+  ctx.beginPath();
+  ctx.arc(cx, cy, radius, 0, Math.PI * 2);
+  ctx.fillStyle = style;
+  ctx.fill();
+  ctx.closePath();
 };
 
 (() => {
-  // localStorage.clear();
-
   let canvas = document.getElementById("window") as HTMLCanvasElement | null;
-  if (canvas === null) {
-    throw new Error("Could not find window");
-  }
+  if (canvas === null) throw new Error("Canvas with id `window` not found");
 
   let ctx = canvas.getContext("2d") as CanvasRenderingContext2D | null;
-  if (ctx === null) {
-    throw new Error("Unsupported '2d' context");
-  }
+  if (ctx === null) throw new Error("2d context not supported");
 
-  const COLORS: Array<string> = [
-    "#01BAEF",
-    "#FFF275",
-    "#DBBADD",
-    "#643A71",
-    "#C4D6B0",
-    "#AA4465",
-  ];
-
-  const ID = uuid();
-  const COLOR: string = choose(COLORS) ?? "magenta";
+  const COLOR: string = choose(COLORS) || "magenta";
+  const ID: string = id();
 
   window.onresize = () => {
     ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-
-    let [x, y] = translateCanvasToScreenCoordinates(
-      ctx.canvas,
-      ctx.canvas.width / 2,
-      ctx.canvas.height / 2,
-    );
-
-    let sessions: Array<Session> = getSessions() || new Array<Session>();
-    updateSession(sessions, { id: ID, color: COLOR, x, y });
-
-    sessions = getSessions() || new Array<Session>();
-    console.log(sessions);
   };
-
-  window.addEventListener("beforeunload", () => {
-    let sessions: Array<Session> | null = getSessions();
-    if (!sessions) return;
-
-    sessions = sessions.filter((s) => s.id != ID);
-    saveSessions(sessions);
-  });
-
-  const getSessions = (): Array<Session> | null => {
-    const sessions = localStorage.getItem("sessions");
-    if (sessions) {
-      return JSON.parse(sessions);
-    }
-    return null;
-  };
-
-  const saveSessions = (sessions: Array<Session>) => {
-    const json = JSON.stringify(sessions);
-    localStorage.setItem("sessions", json);
-  };
-
-  const updateSession = (
-    sessions: Array<Session>,
-    session: Session,
-  ): Array<Session> => {
-    for (let i: number = 0; i < sessions.length; i++) {
-      if (sessions[i].id === ID) {
-        sessions[i] = session;
-      }
-    }
-
-    return sessions;
-  };
-
-  ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-
-  let [x, y] = translateCanvasToScreenCoordinates(
-    ctx.canvas,
-    ctx.canvas.width / 2,
-    ctx.canvas.height / 2,
-  );
-
-  let session: Session = {
-    id: uuid(),
-    color: COLOR,
-    x,
-    y,
-  };
-
-  let sessions: Array<Session> = getSessions() || new Array<Session>();
-
-  sessions.push(session);
-
-  saveSessions(sessions);
-
-  console.log(getSessions());
 
   const render = () => {
-    circle(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2, 100, COLOR);
+    drawCircle(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2, 100, COLOR);
 
     requestAnimationFrame(render);
   };
